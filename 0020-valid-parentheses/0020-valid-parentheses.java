b16be2529ff378fe5a6a41cf71c00923432a661c
PARIDHI SHUKLA <shuklaparidhi2005@gmail.com>
2026-08-25 15:48:22 +0530

    Time: 3 ms (86.11%), Space: 43.1 MB (73.12%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+
+            // Opening bracket
+            if (c == '(' || c == '[' || c == '{') {
+                st.push(c);
+            }
+
+            // Closing bracket
+            else {
+                if (st.isEmpty()) {
+                    return false;
+                }
+
+                char top = st.pop();
+
+                if ((c == ')' && top != '(') ||
+                    (c == ']' && top != '[') ||
+                    (c == '}' && top != '{')) {
+                    return false;
+                }
+            }
+        }
+
+        return st.isEmpty();
+    }
+}
