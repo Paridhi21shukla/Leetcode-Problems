7badc36b5c4da488af413f71737c817ed03a6d16
PARIDHI SHUKLA <shuklaparidhi2005@gmail.com>
2026-09-10 11:55:56 +0530

    Time: 1 ms (100%), Space: 42.6 MB (80.46%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,5 +1,10 @@
-class Solution {
-    public int arrangeCoins(int n) {
-        return (int)((Math.sqrt(8L * n + 1) - 1) / 2);
+// class Solution {
+//     public int arrangeCoins(int n) {
+//         return (int)((Math.sqrt(8L * n + 1) - 1) / 2);
+//     }
+// }
+class Solution{
+    public int arrangeCoins(int n){
+        return (int)((Math.sqrt(8L * n+1)-1)/2);
     }
 }
